76383850f3b2b992eaacb759540ae22cb5df9d3e
hmzsjd <github@hamzasajid.com>
2022-01-14 15:50:12 -0700

    initial commit - server running

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,20 @@
-// Write your "projects" router here!
+const express = require("express");
+const Projects = require("./projects-model");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Projects.get()
+    .then((projArray) => {
+      console.log(projArray);
+      res.status(200).json(projArray);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the projects",
+      });
+    });
+});
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,46 @@
 const express = require('express');
+
+const actionsRouter = require('./actions/actions-router');
+const projectsRouter = require('./projects/projects-router');
+
 const server = express();
+server.use(express.json())
+
+// server.use('/api/actions', actionsRouter)
+server.use('/api/projects', projectsRouter)
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>SPRINT CHALLENGE</h>
+    <p>Welcome to the SPRINT CHALLENGE API</p>
+  `)
+})
+
+module.exports = server;
+
+
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
-module.exports = server;
+
+// const express = require('express')
+// const adoptersRouter = require('./adopters/adopters-router')
+// const dogsRouter = require('./dogs/dogs-router')
+
+// const server = express()
+// server.use(express.json())
+
+// server.use('/api/adopters', adoptersRouter)
+// server.use('/api/dogs', dogsRouter)
+
+// server.get('/', (req, res) => {
+//   res.send(`
+//     <h2>Shelter API</h>
+//     <p>Welcome to the Shelter API</p>
+//   `)
+// })
+
+// module.exports = server
