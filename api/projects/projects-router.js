98a22de71907c20b39e2576d2ad01ef2cfdc3448
hmzsjd <github@hamzasajid.com>
2022-01-14 16:26:32 -0700

    projects router complete

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -35,28 +35,101 @@ router.get("/:id", (req, res) => {
 });
 
 // CREATE A NEW PROJECT
-router.post('/', (req, res) => {
-    const {name, description, completed} = req.body;
+router.post("/", (req, res) => {
+  const { name, description, completed } = req.body;
 
-    if (!name || !description) {
-        res.status(400).json({ message: 'Please provide name and description for the project' })
+  if (!name || !description) {
+    res
+      .status(400)
+      .json({ message: "Please provide name and description for the project" });
+  } else {
+    Projects.insert({ name, description, completed })
+      .then((newProject) => {
+        return Projects.get(newProject.id);
+      })
+      .then((newP) => {
+        res.status(201).json(newP);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "There was an error while saving the post to the database",
+          err: err.message,
+        });
+      });
+  }
+});
+
+// UPDATE PROJECT BY ID
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, description, completed } = req.body;
+
+  if (!name || !description) {
+    res
+      .status(400)
+      .json({ message: "Please provide name and description for the project" });
+  } else {
+    Projects.update(id, { name, description, completed })
+      .then((updatedP) => {
+        res.status(201).json(updatedP);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "The project information could not be updated",
+          err: err.message,
+        });
+      });
+  }
+});
+
+// DELETE PROJECT BY ID
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  Projects.get(id)
+    .then((toBeDeleted) => {
+      if (!toBeDeleted) {
+        res
+          .status(404)
+          .json({ message: "The project with the specified ID does not exist" });
       } else {
-        Projects.insert({name, description, completed})
-        .then(newProject => {
-            return Projects.get(newProject.id)
+        Projects.remove(id)
+        .then(() => {
+          res.json();
         })
-        .then(newP => {
-            res.status(201).json(newP)
-        })
-        .catch(err => {
-            res.status(500).json({
-                message: 'There was an error while saving the post to the database',
-                err: err.message
-              })
-            
-        })
+        .catch((err) => {
+          res.status(500).json({
+            message: "Post could not be deleted",
+            err: err.message,
+          });
+        });
       }
-     
-  })
+      
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "The post could not be removed",
+        err: err.message,
+      });
+    });
+});
+
+// GET ACTIONS BY PROJECT ID
+router.get("/:id/actions", (req, res) => {
+    Projects.getProjectActions(req.params.id)
+      .then((actions) => {
+        if (actions) {
+          res.status(200).json(actions);
+        } else {
+          res.status(404).json({ message: "Project with specified ID not found" });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({
+          message: "Error retrieving the adopter",
+        });
+      });
+  });
 
 module.exports = router;
